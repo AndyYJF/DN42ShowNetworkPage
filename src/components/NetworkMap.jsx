769bd61ee31d009e578e4d9daa6e9ad2c8dcd4c5
AndyYJF <your-email@example.com>
2026-02-16 14:13:15 +0800

    恢复使用 CartoDB 地图瓦片服务

--- src/components/NetworkMap.jsx
+++ src/components/NetworkMap.jsx
@@ -26,10 +26,11 @@ export default function NetworkMap() {
       zoomControl: true,
     });
 
-    // Add GeoQ Dark tile layer (国内访问更快)
-    L.tileLayer('https://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}', {
-      attribution: '&copy; GeoQ',
-      maxZoom: 18,
+    // Add CartoDB Dark Matter tile layer
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20,
     }).addTo(map);
 
     // Custom icon for nodes
